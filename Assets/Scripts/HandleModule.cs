a0127dd11ee0a9295b9b12bfc24e6b48ded4f671
Snack3822 <107415355+Snack3822@users.noreply.github.com>
2022-10-15 11:26:49 +0900

    ハンドルモジュールの追加

--- Assets/Scripts/HandleModule.cs
+++ Assets/Scripts/HandleModule.cs
@@ -7,78 +7,129 @@ public class HandleModule : MonoBehaviour
 {
     public bool completed = false;
 
-    [SerializeField] private GameObject handlePre;
-    //[SerializeField] private GameObject light1;
-    //[SerializeField] private GameObject light2;
-    //[SerializeField] private GameObject light3;
+    [SerializeField] private GameObject handleObject;
+    [SerializeField] private GameObject Leftbutton;
+    [SerializeField] private GameObject Rightbutton;
+    [SerializeField] private GameObject diamondlamp;
+    [SerializeField] private GameObject lampUpper;
+    [SerializeField] private GameObject lampMiddle;
+    [SerializeField] private GameObject lampLower;
 
-    //ハンドルの実体
-    private GameObject handleObject = null;
+    [SerializeField] private GameObject clearObject;
+
+    //マウスカーソル制御用
+    private Vector3 mouse;
 
     //謎解き用の変数
-    private int angle = 0 ;
+    private int angle = 0;
+    private int degree = 0;
+    private int sumdegree = 0;
+    private int n;
+    private bool stage1 = false;
+    private bool stage2 = false;
+    private bool stage3 = false;
 
-    //ボタン制御用の変数
-    private string Directions;
-    private bool isClick;
+    //ピンク色の生成
+    Color pink = new Color(0.933f, 0.509f, 0.933f, 1.0f);
 
     //コルーチン(非同期処理)
     bool coroutineBool = false; 
-
-    //ボタン制御用
-    public void ButtonLeft(){
-        Directions = "L";
-        isClick = true;
-    }
-    public void ButtonRight(){
-        Directions = "R";
-        isClick = true;
-    }
-
 
     // Start is called before the first frame update
     void Start()
     {
         //初期状態の設定
-        handleObject = GameObject.Instantiate<GameObject>(handlePre);
-        //int num = Random.Range(0,0);
+        clearObject.SetActive(false);
+        lampUpper.GetComponent<Renderer>().material.color = Color.black;
+        lampMiddle.GetComponent<Renderer>().material.color = Color.black;
+        lampLower.GetComponent<Renderer>().material.color = Color.black;
+
+        int num = Random.Range(0,6);
+        n = num;
+        degree = LampColor(num);
+        sumdegree = degree;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isClick == true){
-
-            if (Directions == "R")
-            {
-                if (!coroutineBool)
-                {
-                    coroutineBool = true;
-                    isClick = true;
-                    StartCoroutine("RightMove");
-                    angle += 90 ;
-                }
-            }
-            
-            if (Directions == "L")
-            {
-                if (!coroutineBool)
-                {
-                    coroutineBool = true;
-                    isClick = true;
-                    StartCoroutine("LeftMove");
-                    angle -= 90 ;
-                }
-            }
-            //Debug.Log("angle = " + angle);
-
-                //謎解き　その1
-                /*
-                if(num == 0){
-                    if(angle == 270){
+        mouse = Input.mousePosition;
+        Debug.Log("degree = "+ degree +" sumdegree = "+ sumdegree +"angle ="+ angle);
+        Debug.Log("stage1 = "+ stage1 +" stage2 = "+stage2 +" stage3 = "+stage3);
+        /*
+        if(stage2 == true){
+                        Debug.Log("stage2 == true");
+                    }else if(stage2 == false){
+                        Debug.Log("stage2 == false");
+                    }else{
+                        Debug.Log("エラー");
                     }
+                    */
+
+        if(Input.GetMouseButtonDown(0)){
+            //レイの生成
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit, 1000.0f)){
+                //Debug.DrawRay(ray.origin, ray.direction*1000, Color.red, 5, false);
+                if(hit.collider.name == "ButtonLeft"){
+                    if (!coroutineBool)
+                    {
+                        coroutineBool = true;
+                        StartCoroutine("LeftMove");
+                        angle -= 90 ;
+                    }
+                }else if(hit.collider.name == "ButtonRight"){
+                    if (!coroutineBool)
+                    {
+                        coroutineBool = true;
+                        StartCoroutine("RightMove");
+                        angle += 90 ;
+                    }
+                }else if(hit.collider.name == "AnswerButton"){
+                    if(stage1 == false){
+                        if(angle == degree){
+                            lampLower.GetComponent<Renderer>().material.color = Color.green;
+                            int num = 0;
+                                if  (n <= 2){
+                                    num = Random.Range(3,6);
+                            }else if(3 <= n){
+                                    num = Random.Range(0,2);
+                            }
+                            degree = LampColor(num);
+                            n = num;
+                            sumdegree = sumdegree + degree;
+                            stage1 = true;
+                        }
+                    }else if(stage1 == true && stage2 == false){
+                            if(sumdegree == angle){
+                                lampMiddle.GetComponent<Renderer>().material.color = Color.green;
+                                int num = 0;
+                                if  (n <= 2){
+                                    num = Random.Range(3,6);
+                            }else if(3 <= n){
+                                    num = Random.Range(0,2);
+                            }
+                                degree = LampColor(num);
+                                sumdegree = sumdegree + degree;
+                                stage2 = true;
+                                }
+                    }else if(stage1 == true && stage2 == true){
+                        if(sumdegree == angle){
+                            lampUpper.GetComponent<Renderer>().material.color = Color.green;
+                            stage3 = true;
+                            completed = true;
+                        }
+                    }
+                Debug.Log("エラー");
                 }
-                */
+            }
+                
+        }
+        if(completed == true){
+            clearObject.SetActive(true);
+            return;
         }
     }
 
@@ -90,7 +141,6 @@ public class HandleModule : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
         }
         coroutineBool = false;
-        isClick = false;
     }
 
     IEnumerator LeftMove()
@@ -101,6 +151,30 @@ public class HandleModule : MonoBehaviour
             yield return new WaitForSeconds(0.01f);
         }
         coroutineBool = false;
-        isClick = false;
+    }
+
+    //ダイヤランプ色決定関数
+    int LampColor(int n){
+        int angle = 0;
+        if(n == 0){
+            diamondlamp.GetComponent<Renderer>().material.color = pink;
+            angle = 270;
+        }else if(n == 1){
+            diamondlamp.GetComponent<Renderer>().material.color = Color.blue;
+            angle = 360;
+        }else if(n == 2){
+            diamondlamp.GetComponent<Renderer>().material.color = Color.green;
+            angle = 180;
+        }else if(n == 3){
+            diamondlamp.GetComponent<Renderer>().material.color = Color.white;
+            angle = -360;
+        }else if(n == 4){
+            diamondlamp.GetComponent<Renderer>().material.color = Color.red;
+            angle = -270;
+        }else if(n == 5){
+            diamondlamp.GetComponent<Renderer>().material.color = Color.yellow;
+            angle = -180;
+        }
+        return angle;
     }
 }
